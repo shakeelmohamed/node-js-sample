09b7069585f6741e07e9816f05b199c3243bda96
Shakeel Mohamed <smohamed@splunk.com>
2016-08-30 13:10:39 -0700

    Add interval counter log statements

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+var count = 0;
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+  setInterval(function(){
+    count++;
+    console.log("Interval has run " + count + " times!");
+  }, 500);
 })
